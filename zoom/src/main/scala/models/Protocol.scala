d07bcbd10c76bc2752212b6c0245d05c336d7ff3
Jonathan Winandy <jonathan.winandy@gmail.com>
2018-01-05 12:26:23 +0100

    [ten] fix build

--- zoom/src/main/scala/models/Protocol.scala
+++ zoom/src/main/scala/models/Protocol.scala
@@ -6,21 +6,20 @@ import shapeless.Id
 
 import scala.language.higherKinds
 
-
 object TestModel {
 
   trait Logger[F[_]] {
-    protected def log(str: => String, level: Level, callsite: Callsite): F[Unit]
+    protected def log(str: ⇒ String, level: Level, callsite: Callsite): F[Unit]
 
     import Logger._
 
-    def debug(str: => String)(implicit callsite: Callsite): F[Unit] = log(str, Debug, callsite)
+    def debug(str: ⇒ String)(implicit callsite: Callsite): F[Unit] = log(str, Debug, callsite)
 
-    def info(str: => String)(implicit callsite: Callsite): F[Unit] = log(str, Info, callsite)
+    def info(str: ⇒ String)(implicit callsite: Callsite): F[Unit] = log(str, Info, callsite)
 
-    def warn(str: => String)(implicit callsite: Callsite): F[Unit] = log(str, Warn, callsite)
+    def warn(str: ⇒ String)(implicit callsite: Callsite): F[Unit] = log(str, Warn, callsite)
 
-    def error(str: => String)(implicit callsite: Callsite): F[Unit] = log(str, Error, callsite)
+    def error(str: ⇒ String)(implicit callsite: Callsite): F[Unit] = log(str, Error, callsite)
   }
 
   object Logger {
@@ -34,7 +33,6 @@ object TestModel {
     case object Warn extends Level
 
     case object Error extends Level
-
 
     object ops {
       implicit def toDslLogger[F[_]](o: Logger.type)(implicit m: Logger[F]): Logger[F] = m
@@ -57,7 +55,6 @@ object TestModel {
       implicit def toDslEventPublisher[F[_]](o: EventPublisher.type)(implicit m: EventPublisher[F]): EventPublisher[F] = m
     }
 
-
     def apply[F[_]](implicit m: EventPublisher[F]): EventPublisher[F] = m
   }
 
@@ -69,26 +66,24 @@ object TestModel {
 
   implicit val eventSerializer: EventSerializer[String] = new EventSerializer[String] {}
 
-  def prog[F[_] : Monad : Logger : EventPublisher](x: String): F[Unit] = {
+  def prog[F[_]: Monad: Logger: EventPublisher](x: String): F[Unit] = {
 
     for {
-      _ <- Logger.info("test")
-      _ <- Logger.warn("hello")
-      _ <- EventPublisher.publish("abc")
+      _ ← Logger.info("test")
+      _ ← Logger.warn("hello")
+      _ ← EventPublisher.publish("abc")
     } yield {}
   }
 
-
   def main(args: Array[String]): Unit = {
 
-
     implicit val logger: Logger[Id] = new Logger[Id] {
-      override protected def log(str: => String, level: Level, callsite: Callsite): Id[Unit] = {
+      override protected def log(str: ⇒ String, level: Level, callsite: Callsite): Id[Unit] = {
         println("log : " + callsite + " - " + level + " - " + str)
       }
     }
 
-    implicit val eventPublisher:EventPublisher[Id] = new EventPublisher[Id] {
+    implicit val eventPublisher: EventPublisher[Id] = new EventPublisher[Id] {
       override def publish[E](e: E)(implicit eventSerializer: EventSerializer[E], callsite: Callsite): Id[Unit] = {
         println("publish : " + callsite + " - " + e)
 
@@ -97,6 +92,5 @@ object TestModel {
 
     prog[Id]("yo")
   }
-
 
 }
